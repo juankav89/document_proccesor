dbdb6eb2ee505ec4b9736e9c60e2e046be3a996b
Juan Carlos Alfonso Vergel <juankav89@MacBook-Pro-de-Juan.local>
2023-04-08 09:21:31 -0500

    Include base changes to refactor reader process

--- app.py
+++ app.py
@@ -6,27 +6,29 @@ from src.application.writer import Writer
 
 app = Flask(__name__)
 
-root_document = {
-    "name": "root_document",
-    "text": "Some text here",
-    "sections": [
-        {
-            "name": "subtitle_1",
-            "text": "Text for title 1",
-            "sections": [
-                {
-                    "name": "title_1",
-                    "text": "Text for title 1",
-                    "sections": []
-                }
-            ]
-        },
-        {
-            "name": "subtitle_2",
-            "text": "Text for Subtitle 2",
-            "sections": []
-        }
-    ]
+virtual_db = {
+    "root_document": {
+        "name": "root_document",
+        "text": "Some text here",
+        "sections": [
+            {
+                "name": "subtitle_1",
+                "text": "Text for title 1",
+                "sections": [
+                    {
+                        "name": "title_1",
+                        "text": "Text for title 1",
+                        "sections": []
+                    }
+                ]
+            },
+            {
+                "name": "subtitle_2",
+                "text": "Text for Subtitle 2",
+                "sections": []
+            }
+        ]
+    }
 }
 
 
@@ -39,14 +41,14 @@ def init():
 @api_response_handler
 def get_document():
     __args = request.args.to_dict()
-    api_manager = Reader(db=root_document)
+    api_manager = Reader(db=virtual_db)
     return api_manager.get_document(__args)
 
 @app.post("/document")
 @api_response_handler
 def add_document():
     __args = request.get_json()
-    api_manager = Writer(db=root_document)
+    api_manager = Writer(db=virtual_db)
     api_manager.add_document(__args)
-    print(root_document)
+    print(virtual_db)
     return "ok"
